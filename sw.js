cdda1bd76695410cd95eeaa58c994d61d9790f47
ahm-0 <2.20185223e+08+ahm-0@users.noreply.github.com>
2026-08-26 19:25:44 +0000

    Fix Arabic countdown brand rendering

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '2026-08-26-19';
+const CACHE_VERSION = '2026-08-26-20';
 const STATIC_CACHE = `nabd-static-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `nabd-runtime-${CACHE_VERSION}`;
 
