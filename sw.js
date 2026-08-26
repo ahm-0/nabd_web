9dd7bb8bef68353bcf60c4fe0f3c3fe02723e120
ahm-0 <2.20185223e+08+ahm-0@users.noreply.github.com>
2026-08-26 18:23:45 +0000

    Remove countdown tip section

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '2026-08-26-17';
+const CACHE_VERSION = '2026-08-26-18';
 const STATIC_CACHE = `nabd-static-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `nabd-runtime-${CACHE_VERSION}`;
 
