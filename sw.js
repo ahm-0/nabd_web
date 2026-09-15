ea9eaf190c989a9b775621325107eb70d936c8c6
ur7ali <2.29647088e+08+ur7ali@users.noreply.github.com>
2026-09-15 19:55:41 +0000

    feat: improve UI contrast and performance

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '2026-08-31-01';
+const CACHE_VERSION = '2026-09-15-02';
 const STATIC_CACHE = `nabd-static-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `nabd-runtime-${CACHE_VERSION}`;
 
