76bbdf38da7738b6b9ba2b054c6e4a06875a89a9
ahm-0 <2.20185223e+08+ahm-0@users.noreply.github.com>
2026-09-23 05:58:12 +0000

    Remove empty chat announcement strip

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '2026-09-23-19';
+const CACHE_VERSION = '2026-09-23-20';
 const STATIC_CACHE = `nabd-static-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `nabd-runtime-${CACHE_VERSION}`;
 
