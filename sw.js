21baf71efac2e5d07401ed0ee1a3918dc10d0463
ahm-0 <2.20185223e+08+ahm-0@users.noreply.github.com>
2026-09-23 05:19:18 +0000

    Prevent regular users deleting others chat messages

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '2026-09-23-07';
+const CACHE_VERSION = '2026-09-23-08';
 const STATIC_CACHE = `nabd-static-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `nabd-runtime-${CACHE_VERSION}`;
 
